775a97629479ffe70cea80fe553b1c052ee187ff
imrhsohan <imrhsohan@gmail.com>
2023-11-04 03:21:20 +0600

    First Commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(255, 210, 35, 35);
+const Color tdBlue = Color.fromARGB(255, 81, 73, 137);
+
+const Color tdBlack = Color.fromARGB(255, 0, 0, 0);
+const Color tdGrey = Color.fromARGB(255, 115, 103, 62);
+
+const Color tdBGColor = Color.fromARGB(255, 238, 238, 238);
---
+++ lib/model/todo.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true),
+      ToDo(id: '02', todoText: 'Drink Water', isDone: true),
+      ToDo(
+        id: '03',
+        todoText: 'Check Emails',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'Team Meeting',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'Coding Practice',
+      ),
+    ];
+  }
+}
